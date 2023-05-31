25ef6370c944d96025a2bdaf41612f6c16cbd264
leewonho0987 <122905152+leewonho0987@users.noreply.github.com>
2023-05-31 23:13:50 +0900

    LSP3
    리스코프 교체 원칙

---
+++ Lecturer.java
@@ -0,0 +1,15 @@
+package LSP3;
+
+public abstract class Lecturer {
+
+	public Lecturer()
+	{
+		System.out.println("[리스코프 교체의 원칙]");
+	}
+	
+	public String lecturer;
+	public int charge = 90000;
+	public void Lecturer()
+	{}
+	
+}
